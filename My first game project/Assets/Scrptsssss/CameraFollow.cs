1e98d4d98cada9bdc9d420172931b7c1d33b74df
Zeke <ezekiel.w967@stu.nebo.edu>
2021-09-10 14:19:55 -0600

    All da fixes

--- My first game project/Assets/Scrptsssss/CameraFollow.cs
+++ My first game project/Assets/Scrptsssss/CameraFollow.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject tank;
+    private Vector3 offset = new Vector3(0,50,-80);
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = tank.transform.position + offset;
     }
 }
