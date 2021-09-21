a12356d987170c02366c4e5a29ae2ddeadec0ff0
Zeke <ezekiel.w967@stu.nebo.edu>
2021-09-21 14:23:16 -0600

    ezpzezpzezpz

--- prototype 2/Assets/scripts/PlayerController.cs
+++ prototype 2/Assets/scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 2.0f;
+    public float speed = 5.0f;
     public float turnspeed = 10.0f;
     private float hInput;
     private float vInput;
+
+    public float xRange = 9.17f;
+    public float yRange = 4.45f;
 
 
 
@@ -20,6 +23,25 @@ public class PlayerController : MonoBehaviour
         // Makes the Pc rotate left and right
         transform.Rotate(Vector3.forward, turnspeed * speed * Time.deltaTime * hInput);
         // Makes the pc move forward and back
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+        transform.Translate(Vector3.up * speed * Time.deltaTime * vInput);
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } 
+
+         if(transform.position.y > yRange)
+        {
+            // Vector3(x,y,z)
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
     }
-}
+} 
