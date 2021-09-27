df3f831e26f53a8085d3feb90b144d7cd95cc292
Zeke <ezekiel.w967@stu.nebo.edu>
2021-09-27 13:45:17 -0600

    enemy

--- prototype 2/Assets/scripts/EnemyAI.cs
+++ prototype 2/Assets/scripts/EnemyAI.cs
@@ -7,13 +7,15 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
     }
 
     void OnTriggerEnter2D(Collider2D other)
