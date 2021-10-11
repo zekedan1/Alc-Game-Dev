bdce3713ba90f8b434bfa848d73a3a5a852f8c40
Zeke <ezekiel.w967@stu.nebo.edu>
2021-10-11 13:40:51 -0600

    proto 3

--- prototype 3 - First Person Game A/Assets/Scripts/PlayerController.cs
+++ prototype 3 - First Person Game A/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     // movement
     public float moveSpeed;
-    
+    public float jumpForce;
     // Camera
     public float lookSensitivity; // mouse look sensitivity
     public float maxLookX; // lowest down position we can look
@@ -31,6 +31,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        CamLook();
+        if(Input.GetButtonDown("Jump"))
+        Jump();
     }
     void Move()
     {
@@ -38,13 +41,29 @@ public class PlayerController : MonoBehaviour
 
         float z = Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        //rb.velocity = new Vector3(x, rb.velocity.y, z);
+        //Face the directin of the camera
+        Vector3 dir = transform.right * x   + transform.forward * z;
+        //jump direcrtion
+        dir.y = rb.velocity.y;
+        // move in the direction camera
+        rb.velocity = dir;
 
+
+    }
+
+    void Jump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if(Physics.Raycast(ray, 1.1f))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
+        }
     }
     void CamLook()
     {
         float y = Input.GetAxis("Mouse X") * lookSensitivity;
-        rotX = Input.GetAxis("Mouse Y") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
 
         rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
         //Apply Rotation to camera
