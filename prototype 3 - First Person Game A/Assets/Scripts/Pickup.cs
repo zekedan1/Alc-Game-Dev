a1a70c8c1c8cbd659500e28d2010095ee4326dca
Zeke <ezekiel.w967@stu.nebo.edu>
2021-11-15 13:40:49 -0700

    it work

--- prototype 3 - First Person Game A/Assets/Scripts/Pickup.cs
+++ prototype 3 - First Person Game A/Assets/Scripts/Pickup.cs
@@ -51,13 +51,14 @@ public class Pickup : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+     void Update()
     {
-        //Rotating 
+        //Rotating
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
 
-        Vector3 offset = (bobbingUp == true ? new Vector3(0,bobHeight /2, 0) : new Vector3(0, -bobHeight /2, 0));
-        transform.position == Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+        //Bob Up and Down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0,bobHeight / 2, 0) : new Vector3(0, -bobHeight /2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
 
         if(transform.position == startPos + offset)
             bobbingUp = !bobbingUp;
