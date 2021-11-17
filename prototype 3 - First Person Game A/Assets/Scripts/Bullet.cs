952bd8624d147daabd341a8a05be94dd6c496567
Zeke <ezekiel.w967@stu.nebo.edu>
2021-11-17 14:20:01 -0700

    he done shoots himeself

--- prototype 3 - First Person Game A/Assets/Scripts/Bullet.cs
+++ prototype 3 - First Person Game A/Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
     public int damage;
     public float lifetime;
     private float shootTime;
+    public GameObject hitParticle;
 
     void OnEnable()
     {
@@ -28,7 +29,10 @@ public class Bullet : MonoBehaviour
             if(other.CompareTag("Enemy"))
             other.GetComponent<Enemy>().TakeDamage(damage); 
         //Disable the shields lol
-        gameObject.SetActive(false);    
+        gameObject.SetActive(false);
+        // creat hit particle effect
+        GameObject obj = Instantiate(hitParticle, transform.position, Quaternion.identity);
+        Destroy(obj, 0.25f);
     }
     // Update is called once per frame
     void Update()
