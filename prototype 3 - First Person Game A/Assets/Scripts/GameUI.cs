5676b15b01fabbbd2e3c5a3ccd30c013d5e13a15
Zeke <ezekiel.w967@stu.nebo.edu>
2021-12-08 14:20:38 -0700

    UI

---
+++ prototype 3 - First Person Game A/Assets/Scripts/GameUI.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    [Header("Hud")]
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI ammoText;
+    public Image healthBarFill;
+
+    [Header("Pause Menu")]
+    public GameObject pauseMenu;
+
+    [Header("End Game Screen")]
+    public GameObject endGameScreen;
+    public TextMeshProUGUI endGameHeaderText;
+    public TextMeshProGUI endGameScoreText;
+
+    // Instance
+    public static GameUI instance;
+
+    void Awake()
+    {
+        //Set the Instance to this script 
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealthBar(int curHP, int maxHP)
+    {
+        healthBarFill.fillAmount = (float)curHP / (float)maxHP;
+    }
+
+    public UpdateScoreText(int score)
+    {
+        scoreText.text = "Score:" + score;
+    }
+
+    public UpdateAmmoText(int curAmmo, int maxAmmo)
+    {
+        ammoText.text = "Ammo: " + curAmmo + " / " + maxAmmo;
+    }
+
+    public void TogglePauseMenu(bool paused)
+    {
+        pauseMenu.SetActive(pause);
+    }
+
+    public void SetEndGameScreen(bool won, int score)
+    {
+        endGameScreen.SetActive(true);
+        endGameHeaderText.text = won == true ? "You Win, Go to Taco Bell" : "You Lose, No Taco Bell for You!!";
+        endGameHeaderText.color = won == true ? Color.green : Color.red;
+        endGameScoreText.text = "<b>Score</b>\n" + score;
+    }
+
+    public void OnResumeButton()
+    {
+
+    }
+    public void OnRestartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnMenuButton()
+    {
+        SceneManage.LoadScene("Menu");
+    }
+}
